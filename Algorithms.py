3c973289e062db899778a7c2ee73cfb7164f0a7b
Albion Berisha <57451316+Albionn1@users.noreply.github.com>
2022-01-24 21:13:42 +0100

    Shtimi i Kodit krah per krah me Algoritem perkates

--- Algorithms.py
+++ Algorithms.py
@@ -1,3 +1,5 @@
+import code
+from side_by_side import print_side_by_side
 #marrja e listes nga Perdoruesi
 lista = []
 n = int(input("Gjatesia e vargut: "))
@@ -111,15 +113,127 @@ zgjedhja = int(input("""
 """))
 
 if zgjedhja == 1:
-    print("Array i sortuar:")
+    code_ToPrintQuickSort = """
+------------------------------ Kodi i Algoritmit QuickSort ------------------------------
+
+\ndef quicksort(lista):
+    length = len(lista)     
+    if length <= 1:         
+        return lista        
+    else:                   
+        pivot = lista.pop() 
+
+    items_greater = [] 
+                        
+    items_lower = []    
+
+    for item in lista:                  
+        if item > pivot:                
+            items_greater.append(item) 
+        else:                           
+            items_lower.append(item)    
+
+    return quicksort(items_lower) + [pivot] + quicksort(items_greater) # Kthen arrayn e sortuar
+
+------------------------------ Perfundimi i kodit te Algoritmit QuickSort ------------------------------
+    """
+    print("---------------------------------------------------------------------------------------------\n\nArray i sortuar:")
+    print(quicksort(lista)),
+    print(code_ToPrintQuickSort)
+
+    #Perseritja e sortimit pas shfaqjes se kodit
+
+    print("---------------------------------------------------------------------------------------------\n\nArray i sortuar:")
     print(quicksort(lista))
+
 elif zgjedhja == 2:
+    code_ToPrintMergeSort = """
+------------------------------ Kodi i Algoritmit MergeSort ------------------------------
+
+\ndef merge(lista, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+ 
+    L = [0] * (n1)  #
+                    # ==> array-at e perkohshem
+    R = [0] * (n2)  #
+ 
+    for i in range(0, n1):      #
+        L[i] = lista[l + i]     #
+                                # ==> kopjimi i te dhenave ne array-at e perkohshem
+    for j in range(0, n2):      #
+        R[j] = lista[m + 1 + j] #
+ 
+    # bashkimi i array-ave arr[l...r]
+    i = 0     # indeksi fillestar i subArrayt te pare
+    j = 0     # indeksi fillestar i subArrayt te dyte
+    k = l     # Indeksi fillestar i subArrayt te bashkuar
+ 
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            lista[k] = L[i]
+            i += 1
+        else:
+            lista[k] = R[j]
+            j += 1
+        k += 1
+ 
+    while i < n1:
+        lista[k] = L[i]
+        i += 1
+        k += 1
+ 
+    while j < n2:
+        lista[k] = R[j]
+        j += 1
+        k += 1
+ 
+def mergeSort(lista, l, r):
+    if l < r:
+ 
+        m = l+(r-l)//2
+ 
+        mergeSort(lista, l, m)
+        mergeSort(lista, m+1, r)
+        merge(lista, l, m, r)
+
+------------------------------ Perfundimi i Algoritmit MergeSort ------------------------------
+    """
     mergeSort(lista, 0, n-1)
-    print("\n\nArray i sortuar:")
+    print("---------------------------------------------------------------------------------------------\n\nArray i sortuar:")
+    for i in range(n):
+        print("%d" % lista[i],end=" ")
+    print(code_ToPrintMergeSort)
+
+    #Perseritja e sortimit pas shfaqjes se kodit
+
+    mergeSort(lista, 0, n-1)
+    print("---------------------------------------------------------------------------------------------\n\nArray i sortuar:")
     for i in range(n):
         print("%d" % lista[i],end=" ")
 elif zgjedhja == 3:
+    code_ToPrintBubbleSort = """
+------------------------------ Kodi i Algoritmit BubbleSort ------------------------------
+
+\ndef bubblesort(lista):
+    n = len(lista)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if lista[j] > lista[j + 1] :
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+
+------------------------------ Perfundimi i Algoritmit BubbleSort ------------------------------
+    """
     bubblesort(lista)
-    print ("Array i sortuar:")
+    print ("---------------------------------------------------------------------------------------------\n\nArray i sortuar:")
     for i in range(len(lista)):
         print(lista[i], end=" ")
+    print(code_ToPrintBubbleSort)
+
+    #Perseritja e sortimit pas shfaqjes se kodit
+
+    bubblesort(lista)
+    print ("---------------------------------------------------------------------------------------------\n\nArray i sortuar:")
+    for i in range(len(lista)):
+        print(lista[i], end=" ")
+input("\nShtypni Enter qe ta terminoni programin")
